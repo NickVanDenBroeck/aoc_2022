46bfdbbb0f8bd408771d857db4cd77ed64039bc1
Nick Van Den Broeck <37626327+NickVanDenBroeck@users.noreply.github.com>
2023-11-25 20:26:48 +0100

    rename Day

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod day1;
-mod day2;
+mod day4;
 mod common;
 
 fn main() -> std::io::Result<()> {
     day1::part1("./src/input/day1.txt");
     day1::part2("./src/input/day1.txt");
-    day2::part1("./src/input/day2.txt");
-    day2::part2("./src/input/day2.txt");
+    day4::part1("./src/input/day2.txt");
+    day4::part2("./src/input/day2.txt");
 
     Ok(())
 }
